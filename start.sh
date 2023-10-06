fd6a0325f2417f3540e3b9f38439fb13ff0b794b
shadowXalone <146691704+shadowXalone@users.noreply.github.com>
2023-10-07 04:12:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/200920082007/TigerShroff.git /TigerShroff
+  git clone https://github.com/shadowXalone/TigerShroff.git /TigerShroff
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Tigershroff
