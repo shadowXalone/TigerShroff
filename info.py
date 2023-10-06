9ab32602fea267f24da4c63ca204084ff541fc57
shadowXalone <146691704+shadowXalone@users.noreply.github.com>
2023-10-07 04:19:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001964966568)
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001964966568')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'HelpDesk_XDs')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
